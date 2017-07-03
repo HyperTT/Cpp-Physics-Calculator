23108625383967524526c793ad8c930f36b6dd9e
HyperTT <HyperTT@users.noreply.github.com>
2017-07-02 23:09:49 -0700

    Create driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "physics.h"
+using namespace std;
+
+int main()
+{
+    cout << find_ke_value(10, 45.4) << endl;
+    cout << find_ke_value(683, 3) << endl;
+    cout << find_ke_value(189, 87) << endl;
+    cout << find_ke_value(13, 4.7865) << endl;
+
+    return 0;
+}
